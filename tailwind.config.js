6e1fa76d31d7da5adb2b5b7a34eade708177bc0e
ice5000 <95354975+ice5000@users.noreply.github.com>
2023-08-09 08:33:28 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        merriweather: ['Merriweather', 'serif'],
+        merriweather: ['erriweather', 'serif'],
       }
     },
   },
