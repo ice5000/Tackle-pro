b0dd9b11fd8371e0c1eec831ce9738289995ec39
ice5000 <95354975+ice5000@users.noreply.github.com>
2023-08-15 12:27:21 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,51 @@
 import React from "react";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
+import { useCallback, useEffect } from "react";
 
 function Header() {
   const nav = document.getElementsByClassName("sidenav");
-  function openNav() {
+
+  const openNav = useCallback(() => {
     if (nav) {
       nav[0].style.width = "250px";
     }
-  }
+  }, [nav]);
 
-  function closeNav() {
+  const closeNav = useCallback(() => {
+    //event.stopPropagation();
     if (nav) {
       nav[0].style.width = "0px";
     }
-  }
+  }, [nav]);
+
+  useEffect(() => {
+    document.addEventListener("click", (event) => {
+      const navbar = document.getElementById("navbar");
+      // const openButton = document.getElementById("mobile-nav");
+      const target = event.target;
+
+      if (navbar && target !== navbar && !navbar.contains(target)) {
+        closeNav();
+      }
+    });
+    return () => {
+      document.removeEventListener("click", (event) => {});
+    };
+  }, [closeNav, openNav]);
 
   return (
     <>
-      <div className="lg:hidden  fixed w-full h-20 z-20 flex  justify-center items-center bg-[#131b2a]">
+       <div className="lg:hidden  fixed w-full h-20 z-20 flex  justify-center items-center bg-[#131b2a]">
         <div className="flex-row flex space-x-20 justify-center items-center">
           <div
-            className=" text-white"
+            id="mobile-nav"
+            className=" text-white nav-classs"
             style={{ fontSize: "35px", cursor: "pointer" }}
-            onClick={() => openNav()}
+            onClick={(event) => {
+              event.stopPropagation();
+              openNav();
+            }}
           >
             &#9776;
           </div>
@@ -45,13 +67,13 @@ function Header() {
           id="navbar"
           className=" text-white items-center space-y-5  sidenav flex flex-col"
         >
-          <button className="closebtn" onClick={() => closeNav()}>
+           <button className="closebtn" onClick={() => closeNav()}>
             &times;
           </button>
-          <div className=""> Home</div>
-          <div>Tokenomics</div>
-          <div>Whitepaper</div>
-          <div>FaQ</div>
+          <div className="nav-classs"> Home</div>
+          <div className="nav-classs">Tokenomics</div>
+          <div className="nav-classs">Whitepaper</div>
+          <div className="nav-classs">FaQ</div>
           <div>
             <button className="border-2 w-24 rounded-lg ">Buy</button>
           </div>
@@ -91,20 +113,20 @@ function Header() {
               Takoshi is finally the new coach of Tackle City, a struggling
               soccer club in one of Europes's toughest leagues.
             </h1>
-            <div className="text-[16px] justify-center lg:space-y-3  flex lg:text-[14px] flex-col mx-5 ">
-              <p className="lg:max-w-[550px]">
+            <div className="text-[16px] justify-center lg:space-y-2  flex lg:text-[14px] flex-col mx-5 ">
+              <p className="lg:max-w-[550px] max-w-[400px] mb-2 lg:mb-0">
                 Takoshi sets out to find the best soccer talents worldwide and
                 works with each player to coax out their hidden talents and
                 utilize their true potential.
               </p>
-              <p className="lg:max-w-[550px]">
+              <p className="lg:max-w-[550px] max-w-[400px]lg:mb-0 mb-2">
                 Using his intense training camp, unorthodox methods and his
                 knack for "practising for when the going gets tough," Takoshi is
                 here to prove his team's skill against a host of
-                continentalrivals with more capital and larger budgets.
+                continental rivals with more capital and larger budgets.
               </p>
 
-              <p className="lg:max-w-[450px]">
+              <p className="lg:max-w-[450px] lg:mb-0 mb-2">
                 The goal is to discover their true potential and discover the
                 most battle-hardened, self-confident soccer player within the
                 group.
@@ -129,7 +151,7 @@ function Header() {
       </div>
       <div className="bg-[#131b2a] text-white lg:flex-row h-[900px] flex-col flex lg:space-x-24 lg:h-[500px] space-y-10 items-center justify-center ">
         <div className=" flex flex-col space-y-1 items-center">
-          <div className="header-fonts text-[29px] font-bold font-railway lg:text-[30px]">
+          <div className="header-fonts text-[29px] mb-5 font-bold font-railway lg:text-[30px]">
             Tackle, Time to take over
           </div>
           <div className="lg:text-[14px]  text-[16px]">
